4c5d5612ec460f32991e5522d1722fcab7ca5214
carolinahulse <carolinahulse321@gmail.com>
2022-05-31 17:14:08 -0300

    Implementação de códigos nas classes Pessoa, PessoaFisica, PessoaJuridica, Farmaceutico, Fornecedor e execução do sistema

--- src/main/java/br/com/triersistemas/cebolinha/App.java
+++ src/main/java/br/com/triersistemas/cebolinha/App.java
@@ -1,10 +1,12 @@
 package br.com.triersistemas.cebolinha;
-
-import java.util.Arrays;
-
 public class App {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(args));
+    	Fornecedor fornecedor1 = new Fornecedor();
+        Farmaceutico farmaceutico1 = new Farmaceutico();
+        
+        System.out.println("Fornecedor: "+fornecedor1.getNome()+"\nCNPJ: "+fornecedor1.getDocumento()+"\nProduto: "+fornecedor1.getProduto());
+        System.out.println("-------------------------");
+        System.out.println("Farmaceutico: "+farmaceutico1.getNome()+"\nCPF: "+farmaceutico1.getDocumento()+"\nOferta do dia: "+farmaceutico1.getOfertaDoDia());
     }
 }
