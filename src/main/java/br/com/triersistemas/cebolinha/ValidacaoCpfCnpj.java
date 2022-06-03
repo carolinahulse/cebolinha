e8e8cd57c07b8ec3490871ccbbd8b4cb83a788d0
carolinahulse <carolinahulse321@gmail.com>
2022-06-03 17:19:33 -0300

    Criacao da classe de validacao de CPF e CNPJ

---
+++ src/main/java/br/com/triersistemas/cebolinha/ValidacaoCpfCnpj.java
@@ -0,0 +1,7 @@
+package br.com.triersistemas.cebolinha;
+public class ValidacaoCpfCnpj {
+	Integer[] primeiroNumero = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	Integer[] segundoNumero = {0, 2, 3, 4, 5, 6, 7, 8};
+	Integer[] validar;
+	
+}
